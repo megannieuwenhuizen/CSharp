dfb548054abc0f35f4ae032974e9ae98661d489c
Megan Nieuwenhuizen <megan.nieuwenhuizen@gmail.com>
2022-02-08 20:58:49 +0200

    Section 2 exercises
    Assignments - completed
    If and switch basic - completed

---
+++ Projects/1 Basic C# Projects/Section2Assignment2/Section2Assignment2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Section 2 Assignment 2: Time machine
+ Develop an app which takes a date (birthday eg) and it should tell you what day of the week it was*/
+namespace Section2Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime timemachine=new DateTime();
+            Console.WriteLine("Enter the day: " );
+            int.TryParse(Console.ReadLine(), out int day);
+            Console.WriteLine("Enter the month: ");
+            int.TryParse(Console.ReadLine(), out int month);
+            Console.WriteLine("Enter the year: ");
+            int.TryParse(Console.ReadLine(), out int year);
+            timemachine.AddDays(day);
+            timemachine.AddMonths(month);
+            timemachine.AddYears(year);
+            Console.WriteLine("This day should be a " + timemachine.DayOfWeek);
+
+
+        }
+    }
+}
