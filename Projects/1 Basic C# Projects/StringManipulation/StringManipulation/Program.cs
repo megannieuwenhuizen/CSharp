e0cae5ae7321fb1003ab3bfbe9214cdcfb22cb12
Megan Nieuwenhuizen <megan.nieuwenhuizen@gmail.com>
2022-02-15 20:43:36 +0200

    Fundamentals of Programming Part 3
    Constants
    TryParse
    StringManipulation Pt1+Pt2

---
+++ Projects/1 Basic C# Projects/StringManipulation/StringManipulation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string movieName = "   lord of the rings   ";
+            String tvShow = "CSI: New York";
+            //Indexes
+            Console.WriteLine(movieName[0]);
+
+            //Trim
+            Console.WriteLine(movieName.Trim());
+
+            //Upper
+            Console.WriteLine(movieName.ToUpper().Trim());
+
+            //Lower
+            Console.WriteLine(movieName.ToLower().Trim());
+
+            //Length
+            Console.WriteLine(movieName.Length);
+
+            //Replace
+            Console.WriteLine(movieName.Replace('l', 'L'));
+
+            //Index of
+            Console.WriteLine(tvShow.IndexOf('o'));
+
+            //Last Index of
+            Console.WriteLine(tvShow.LastIndexOf('o'));
+
+            //Substring
+            Console.WriteLine(tvShow.Substring(4));
+
+            //Remove
+            Console.WriteLine(tvShow.Remove(4));
+
+            //insert
+            Console.WriteLine(tvShow.Insert());
+        }
+    }
+}
---
+++ Projects/1 Basic C# Projects/TryParse/TryParse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int x);
+            if (x == 0)
+            {
+                Console.WriteLine("This is not a valid input");
+            }
+            else { }
+            Console.WriteLine("You have entered no: " + x);
+        }
+        }
+    }
+}
