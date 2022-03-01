3c71ece434a551d6a0c62c6a6e00521d15aba9db
Megan Nieuwenhuizen <megan.nieuwenhuizen@gmail.com>
2022-03-01 17:08:29 +0200

    Updates
    Section 2 Assignment 1,2,3
    Loops: For, while, do while and nested loops
    LoopsExercise
    StringManupulation exercises
    StringInterpolation

---
+++ Projects/1 Basic C# Projects/Loops/Loops/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //For Loop
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("For Loop:");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("The value of variable i is " + i + " in the for loop.");
+                if (i == 5)
+                    break;
+            }
+
+            //While Loop
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("While Loop:");
+            int x = 1;
+            while (x <= 10)
+            {
+                Console.WriteLine("This is line number " + x + "in the while loop.");
+                x++;
+                if (x == 6)
+                    break;
+            }
+
+            //Do While Loop
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Do While Loop:");
+            int z = 1;
+            do
+            {
+                Console.WriteLine("This is line number " + z + "in the do while loop.");
+                z++;
+                if (z == 9)
+                    break;
+            } while (z <= 10);
+
+            //Nested Loop
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Nested Loops:");
+
+            for (int s = 1; s <= 4; s++)
+            {
+                for (int p = 1; p<=5; p++)
+                {
+                    Console.Write("0 ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("---------------------------------------------");
+        }
+    }
+}
---
+++ Projects/1 Basic C# Projects/LoopsExercise/LoopsExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Even Numbers
+            Console.WriteLine("Even numbers exercise:");
+            for (int i=2; i<=100;i=i+2)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+
+            //Triangle
+            Console.WriteLine("Build a triangle exercise:");
+            for (int j=0; j<=10; j++)
+            {
+                for (int k=1; k<=j+1;k++)
+                {
+                    Console.Write("0 ");
+                }
+                Console.WriteLine();
+
+
+            }
+        }
+    }
+}
