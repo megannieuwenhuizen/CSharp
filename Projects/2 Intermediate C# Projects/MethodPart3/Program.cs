f2bf13392dbd17b42f666c8df15e6d1e20773df6
Megan Nieuwenhuizen <megan.nieuwenhuizen@gmail.com>
2022-04-19 08:32:50 +0200

    Method Exercises
    Methods beginning
    Overloaded Methods

---
+++ Projects/2 Intermediate C# Projects/MethodPart3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPart3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstEmployee, secondEmployee;
+
+            /*1*/firstEmployee = "Shan Will";
+            /*2*/secondEmployee = "Megs Dillon";
+
+            /*3*/Console.WriteLine($"Inside Main Method \n------------------------\n{firstEmployee}\n{secondEmployee}\n\n");
+            /*ORRRRRRRR ignore line 1,2,3 
+             * passing by reference
+             ChangeNames(out string firstEmployee, out string secondEmployee) ;
+             */
+
+
+            ChangeNames(ref firstEmployee,ref secondEmployee);
+            Console.WriteLine($"Inside Main Method \n------------------------\n{firstEmployee}\n{secondEmployee}\n\n");
+        }
+        static void ChangeNames(ref string firstEmp,ref string secEmp)
+        {
+            //passing by value
+            firstEmp = "Fagan Dillon";
+            secEmp = "Kisten Gonsalves";
+            Console.WriteLine($"Outside Main Method \n------------------------\n{firstEmp}\n{secEmp}\n\n");
+        }
+    }
+   
+}
